b0159f63d44328d1e98f96c5ca469fdc65303d9e
Jason <cameljeet@gmail.com>
2016-11-25 13:53:48 -0800

    Make Ajax request in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
+const BASE_URL = 'http://localhost:3001'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      questions: []
+    }
+  }
+
+  getQuestions() {
+    $.ajax({
+      url: `${BASE_URL}/api/v1/questions`,
+      success: function(questions) {
+        this.setState({ questions: questions })
+      }.bind(this)
+    })
+  }
+
+  componentDidMount() {
+    this.getQuestions()
+  }
+
   render() {
     return (
       <div className="App">
+        {
+          JSON.stringify(this.state.questions)
+        }
       </div>
     );
   }
